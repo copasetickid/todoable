1d324a56242e1c0f1819721f53c42e791212334b
Rushaine McBean <rushaine.mcbean@gmail.com>
2019-02-12 08:04:02 -0500

    Load in 3rd party gems and library code to main module

--- lib/todoable.rb
+++ lib/todoable.rb
@@ -1,6 +1,12 @@
+require "httparty"
+require 'dotenv'
 require "todoable/version"
+require "todoable/error"
+require "todoable/client"
+require "todoable/list"
 
 module Todoable
-  class Error < StandardError; end
+  #class Error < StandardError; end
   # Your code goes here...
+  Dotenv.load
 end
