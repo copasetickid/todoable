0641e1cdbe0a3d58bbf922e39efdb9234b87e656
Rushaine McBean <rushaine.mcbean@gmail.com>
2019-02-18 16:47:46 -0500

    Adds tests for destroying a list and editing the name

--- spec/todoable/list_spec.rb
+++ spec/todoable/list_spec.rb
@@ -23,8 +23,7 @@ RSpec.describe Todoable::List do
     end
   end
 
-
-  describe "#find" do 
+  describe "#find" do
     it "returns information on a list with items" do
       VCR.use_cassette("list_lookup") do
         list = Todoable::List.new("name": "list seven", "id": 5)
@@ -34,11 +33,27 @@ RSpec.describe Todoable::List do
       end
     end
   end
+
+  describe "#edit_name" do
+    it "calls the get method from the client" do
+      VCR.use_cassette("list_edit_name") do
+        guitar_list = Todoable::List.new(name: "Guitars", id: 10)
+        params = {
+          "list": { "name": "My Guitars"}
+        }
+        expect(Todoable::List.client).to receive(:patch).with("/lists/10", params)
+        guitar_list.edit_name("My Guitars")
+      end
+    end
+  end
+
+  describe "#destroy" do
+    it "calls the delete method from the client" do
+      VCR.use_cassette("list_destroy") do
+        sneaker_list = Todoable::List.new(name: "Sneakers", id: 11)
+        expect(Todoable::List.client).to receive(:delete).with("/lists/11")
+        sneaker_list.destroy
+      end
+    end
+  end
 end
-
-
-# it "returns Todoable lists" do
-#      list_array = [{"name" => "list one", "id" => 1}]
-#      allow(Todoable::List.connection).to receive(:lists).and_return list_array
-#      expect(Todoable::List.all.first).to be_a Todoable::List
-#    end
